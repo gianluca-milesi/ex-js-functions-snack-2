839a8c49d1cbe909fb00dca3929b00f6ed5b8bee
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2025-02-24 18:34:49 +0100

    ADD: snack 1

---
+++ snacks.js
@@ -0,0 +1,18 @@
+//SNACK 1
+function somma(a, b) {
+    return a + b
+}
+
+console.log(somma(2, 2))
+
+
+const somma = function (a, b) {
+    return a + b
+}
+
+console.log(somma(2, 2))
+
+
+const somma = (a, b) => (a + b)
+
+console.log(somma(2, 2))
